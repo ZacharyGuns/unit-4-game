9dfae2a516711440b7e2383afb52d2d8ddc782d2
The Z-man <zacharyguns@macintosh.attlocal.net>
2018-05-13 21:45:58 -0700

    i hate everything slightly less, but only slightly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // Variables
 var randomNumber = ["19", "120"];
-var matchNumber;
+var currentNumber = 0;
 var yourScoreIs;
 var playerScore = 0;
 var diamond;
@@ -12,14 +12,15 @@ var lossesScore = 0;
 
 //===========================================================================
 // Page text
+document.getElementById("matchNumber").innerHTML = currentNumber;
 document.getElementById("yourScoreIs").innerHTML = "Your current score is: " + playerScore;
-document.getElementById("playerNumber").innerHTML = playerScore;
 document.getElementById("wins").innerHTML = "Wins: " + winsScore;
 document.getElementById("losses").innerHTML = "Losses: " + lossesScore;
 
 //===========================================================================
 // Functions
 
+/*
 function chooseNumber() {
     matchNumber = randomNumber[Math.floor(Math.random() * randomNumber.length)];
 }
@@ -28,3 +29,26 @@ function loopRandomNumber() {
     for (var i = 0; i < randomNumber.length; i++) {
     }
 }
+*/
+
+var counter = 0;
+
+$("#diamond").on("click", function() {
+  counter += 1;
+  alert("You clicked the diamond " + counter + " times!");
+});
+
+$("#ruby").on("click", function() {
+  counter += 1;
+  alert("You clicked the ruby " + counter + " times!");
+});
+
+$("#sapphire").on("click", function() {
+  counter += 1;
+  alert("You clicked the sapphire " + counter + " times!");
+});
+
+$("#emerald").on("click", function() {
+  counter += 1;
+  alert("You clicked the emerald " + counter + " times!");
+});
